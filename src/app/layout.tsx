834687701e7dcd4284d537260b49bd71fe34db49
Shahzaib Anwar <letsmail.shahzaib@gmail.com>
2026-05-19 12:48:13 +0500

    Layout Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import enMessages from "../../messages/en.json";
-import Navbar from "../components/sections/Navbar";
-import Footer from "../components/sections/Footer";
 
 export const metadata: Metadata = {
   title: "Karigaar",
@@ -19,9 +17,7 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased">
       <body className="min-h-full">
         <NextIntlClientProvider locale="en" messages={enMessages}>
-          <Navbar />
           {children}
-          <Footer />
         </NextIntlClientProvider>
       </body>
     </html>
